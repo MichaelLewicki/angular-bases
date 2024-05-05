665fb25f3fdb71120388d6bb4be5084601c635e3
Michael Lewicki <lewickiclass@gmail.com>
2024-05-05 16:46:48 -0400

    clase @input - sección 5

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-list',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent {
+
+  @Input()//si no se declara un nombre. Se adopta el nombre del atributo
+  public characterList : Character[] = [
+    {
+      name: 'Trunks',
+      power: 10
+    }
+  ];//El contenido de este atributo perderá su valor y adoptará el que llega de Input si se asigna ese decorador en el html
 
 }
