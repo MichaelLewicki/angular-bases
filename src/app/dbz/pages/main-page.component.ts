c52eaa14dd76ad778f3b1aa1c6a86dc92c46fcff
Michael Lewicki <lewickiclass@gmail.com>
2024-05-07 19:45:58 -0400

    Servicios - Seccion 5

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Character } from '../interfaces/character.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -7,33 +7,9 @@ import { Character } from '../interfaces/character.interface';
 })
 export class MainPageComponent {
 
-  public characters: Character[] = [
-    {
-      name: 'Krillin',
-      power: 1000,
-    },
-    {
-      name: 'Goku',
-      power: 9500,
-    },
-    {
-      name: 'Vegeta',
-      power: 7500
-    }
-  ];
-
-  public onNewCharacter(character: Character) : void {
-    console.log('MainPage');
-    console.log(character);
-    //this.characters.unshift(character);// .unshift es como .add pero agrega el elemento al principio del arreglo.
-    this.characters.push(character); //.push es lo mismo que .add en Java. Agrega el elemento al final
-  };
-
-  public onDeleteCharacter(index: number) : void {
-    let deletedRows:number = 1;
-    //splice nos permite eliminar un elemento del array mediante su numero de index y la cantidad de elementos que deseamos eliminar
-    this.characters.splice(index, deletedRows);
+  //inyectar service
+  constructor(public dbzService: DbzService) {
+    
   }
-
 
 }
---
+++ src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DbzService {
+
+  public characters: Character[] = [
+    {
+      name: 'Krillin',
+      power: 1000,
+    },
+    {
+      name: 'Goku',
+      power: 9500,
+    },
+    {
+      name: 'Vegeta',
+      power: 7500
+    }
+  ];
+
+  public onNewCharacter(character: Character) : void {
+    console.log('MainPage');
+    console.log(character);
+    //this.characters.unshift(character);// .unshift es como .add pero agrega el elemento al principio del arreglo.
+    this.characters.push(character); //.push es lo mismo que .add en Java. Agrega el elemento al final
+  };
+
+  public onDeleteCharacter(index: number) : void {
+    let deletedRows:number = 1;
+    //splice nos permite eliminar un elemento del array mediante su numero de index y la cantidad de elementos que deseamos eliminar
+    this.characters.splice(index, deletedRows);
+  }
+
+}
